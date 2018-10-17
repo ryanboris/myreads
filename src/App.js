3b3b5b69b0dd02e783c85b74a0d002bdd83763f2
Ryan Boris <neodynia@icloud.com>
2018-10-17 04:01:14 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom'
+import Home from './components/Home'
 
-class App extends Component {
+import './appandindex.css'
+
+export default class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <Route exact path="/" component={Home} />
   }
 }
-
-export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HomeContainer = styled.div`
+   width: 100%;
+`
+
+const Home = (props) => {
+    return <HomeContainer />
+}
+
+export default Home
